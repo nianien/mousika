5325db753e16adf906ebc16170ea400da5f77b7d
liyifei@kuaishou.com <liyifei@kuaishou.com>
2023-12-01 19:01:13 +0800

    升级发包插件

--- mousika-core/src/main/java/com/skyfalling/mousika/udf/UdfDelegate.java
+++ mousika-core/src/main/java/com/skyfalling/mousika/udf/UdfDelegate.java
@@ -1,11 +1,12 @@
 package com.skyfalling.mousika.udf;
 
 import com.cudrania.core.reflection.Reflections;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.skyfalling.mousika.utils.JsonUtils;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.Optional;
-import java.util.function.BiFunction;
 
 /**
  * Udf代理接口, 默认实现类型自动转化
@@ -45,22 +46,6 @@ public interface UdfDelegate<P, R> {
 
     class Wrapper implements UdfDelegate {
 
-        private static BiFunction<Object, Class, Object> converter = (s, t) -> {
-            s = convertIntoJavaObject(s);
-            if (s instanceof Wrapper) {
-                return ((Wrapper) s).udf; //嵌套udf的参数在嵌套方法中已处理
-            }
-            if (!(s instanceof String)) {
-                s = JsonUtils.toJson(s);
-            }
-            Object v = JsonUtils.toBean((String) s, t);
-            return v != null ? v : s;
-        };
-
-        // do ScriptObjectMirror JS -> Java conversion fix array converted map problem
-        private static Object convertIntoJavaObject(Object scriptObj) {
-            return scriptObj;
-        }
 
         /**
          * 代理函数
@@ -82,21 +67,74 @@ public interface UdfDelegate<P, R> {
         @Override
         public Object apply(Object... params) {
             int n = params.length;
-            Optional<Method> found = Reflections.getMethods(udf.getClass(),
-                            m -> m.getName().equals("apply")
-                                    && m.getParameterCount() == n
-                                    && !m.isBridge() //the overloaded method is marked as being a "bridge method".
-                    )
-                    .stream().findFirst();
+            Optional<Method> found = Reflections.getMethods(udf.getClass(), m -> m.getName().equals("apply") && m.getParameterCount() == n && !m.isBridge() //the overloaded method is marked as being a "bridge method".
+            ).stream().findFirst();
             if (!found.isPresent()) {
                 throw new RuntimeException("no compatible method has " + params.length + " parameters!");
             }
             Method method = found.get();
-            Object[] casts = Reflections.convert(params, method.getParameterTypes(), converter);
+            Object[] casts = convert(params, method.getGenericParameterTypes());
             return Reflections.invoke(method, udf, casts);
         }
 
 
-    }
+        public static Object[] convert(Object[] parameters, Type[] parameterTypes) {
+            Object[] castParams = new Object[parameters.length];
+            for (int i = 0; i < parameterTypes.length; i++) {
+                Type parameterType = parameterTypes[i];
+                castParams[i] = convert(parameters[i], parameterType);
+            }
+            return castParams;
+        }
 
+
+        public static Object convert(Object parameter, Type parameterType) {
+            if (parameterType instanceof Class) {
+                Class clazz = (Class) parameterType;
+                if (clazz.isInstance(parameter)) {
+                    return parameter;
+                }
+                if (parameter instanceof String) {
+                    String valueString = (String) parameter;
+                    if (clazz.equals(String.class)) {
+                        return valueString;
+                    }
+                    if (clazz.equals(Boolean.TYPE) || clazz.equals(Boolean.class)) {
+                        return Boolean.valueOf(valueString);
+                    }
+                    if (clazz.equals(Byte.TYPE) || clazz.equals(Byte.class)) {
+                        return Byte.valueOf(valueString);
+                    }
+                    if (clazz.equals(Short.TYPE) || clazz.equals(Short.class)) {
+                        return Short.valueOf(valueString);
+                    }
+                    if (clazz.equals(Integer.TYPE) || clazz.equals(Integer.class)) {
+                        return Integer.valueOf(valueString);
+                    }
+                    if (clazz.equals(Long.TYPE) || clazz.equals(Long.class)) {
+                        return Long.valueOf(valueString);
+                    }
+                    if (clazz.equals(Float.TYPE) || clazz.equals(Float.class)) {
+                        return Float.valueOf(valueString);
+                    }
+                    if (clazz.equals(Double.TYPE) || clazz.equals(Double.class)) {
+                        return Double.valueOf(valueString);
+                    }
+                    if (clazz.equals(Character.TYPE) || clazz.equals(Character.class)) {
+                        return Character.valueOf(valueString.charAt(0));
+                    }
+                    if (clazz.isEnum()) {
+                        return Enum.valueOf((Class<Enum>) clazz, valueString);
+                    }
+                }
+            }
+            return JsonUtils.toBean(JsonUtils.toJson(parameter), new TypeReference<>() {
+                @Override
+                public Type getType() {
+                    return parameterType;
+                }
+            });
+        }
+
+    }
 }
